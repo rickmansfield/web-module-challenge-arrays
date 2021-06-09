17c058c3ead14d8fc97f8fc6f94030b0955ce1bf
Rick <rmansfield@realpeoplesratings.com>
2021-06-09 12:18:24 -0500

    Task 1 complete

--- index.js
+++ index.js
@@ -45,9 +45,12 @@ Use the copy function below to do the following:
     2. Return a copy of the received array  
 */
 
-function copy(/*your code here*/){
-    /*your code here*/
-}    
+// function copy(originalArray){
+//     const newFlavorsList = [...originalArray];
+//     console.log(newFlavorsList);
+// }
+// copy(originalFlavors);
+
 
 
 
@@ -64,9 +67,14 @@ For Example: is31Flavors(originalFlavors) will return true if your code is worki
 */
 
 
-function is31Flavors(/*your code here*/){
-   /*your code here*/
+function is31Flavors(anyArray){
+   if (anyArray.length === 31){
+       return true;
+//    } else {
+//        return false;
+//    }
 }
+console.log(is31Flavors(originalFlavors));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Corporate has come to you with an idea for a new flavor: Rainbow Sherbert! They think this will be a game changer. You need to modify the array to include this flavor. 
@@ -81,9 +89,9 @@ Use the addFlavor function below to do the following:
 */
 
 
-function addFlavor(/*your code here*/){
-   /*your code here*/
-}
+// function addFlavor(array, string){
+//    array.unshift(string);
+// }
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
